408f1f539c9510f5222fe58e85791caffdda1769
Zaky <zaky@Zakys-MacBook-Air.local>
2024-12-12 21:31:48 +0800

    feat: add port on start

--- invoice_ocr/start.py
+++ invoice_ocr/start.py
@@ -1,4 +1,5 @@
 from invoice_ocr.app import app as application
 
 if __name__ == "__main__":
-    application.run()
+    application.run(host=os.getenv('IP', '0.0.0.0'), 
+            port=int(os.getenv('PORT', 444)))
